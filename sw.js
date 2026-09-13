d94d8d8dc0a60ff10c90a9a0d6a3feaab3679b3e
eer2a2 <erhanerbab@gmail.com>
2026-09-13 15:56:42 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ares-english-v61';
+const CORE=['./','./index.html','./speaking.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
+self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const req=event.request;const isPage=req.mode==='navigate'||/\.(?:html?)$/i.test(new URL(req.url).pathname);if(isPage){event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res}).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html'))));return}event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res}))) });
